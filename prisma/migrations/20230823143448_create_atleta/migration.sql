fab81f3949ce65c4a20060c8b813a7f124abed94
Luis Henrique <52759764+luishenriqueramalho@users.noreply.github.com>
2023-08-23 14:01:13 -0300

    new migration

--- prisma/migrations/20230823143448_create_atleta/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Atleta" (
-    "id" TEXT NOT NULL,
-    "nome" TEXT NOT NULL,
-    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Atleta_pkey" PRIMARY KEY ("id")
-);
